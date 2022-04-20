fc174898ce9ce24d87f1f07617009bbf43839c86
Vikas Upadhyay <vikas.92543@gmail.com>
2022-04-20 14:00:07 +0530

    call method between api & mvc

--- MVCWebApplication22/Controllers/HomeController.cs
+++ MVCWebApplication22/Controllers/HomeController.cs
@@ -8,144 +8,11 @@ using MVCWebApplication22.Models;
 
 namespace MVCWebApplication22.Controllers
 {
-    //public class HomeController : Controller
-    //{
-    //    public ActionResult Index()
-    //    {
-    //        return View();
-    //    }
-
-    //    public ActionResult About()
-    //    {
-    //        ViewBag.Message = "Your application description page.";
-
-    //        return View();
-    //    }
-
-    //    public ActionResult Contact()
-    //    {
-    //        ViewBag.Message = "Your contact page.";
-
-    //        return View("Contact");
-    //    }
-
-
-    //    public ActionResult Create()
-    //    {
-    //        ViewBag.Message = "Your contact page.";
-
-    //        return View(new Provider());
-    //    }
-
-    //[HttpPost]
-    //    public ActionResult Create(Provider p)
-    //    {
-    //        if (ModelState.IsValid)
-    //        {
-
-    //            Provider newP = p;
-    //            p.InsertProvider(p);
-    //        }
-
-    //        return RedirectToAction("ShowProviders");
-    //    }
-
-    //public ActionResult Edit(int? id, string name, string type)
-    //{
-    //    ViewBag.Message = "Your contact page.";
-
-    //    return View(new Provider() { ProviderId = id ?? 0, ProviderType = type, ProviderName = name });
-    //}
-
-    //    public ActionResult Details(int? id)
-    //    {
-    //        ViewBag.Message = "Your contact page.";
-
-    //        var result = Provider.GetProviderDataByProviderId(id ?? 0);
-
-
-    //        Provider newP = new Provider()
-    //        {
-    //            ProviderId = Convert.ToInt32(result.Tables[0].Rows[0][0].ToString()),
-    //            ProviderName = result.Tables[0].Rows[0]["ProviderName"].ToString(),
-    //            ProviderType = result.Tables[0].Rows[0]["ProviderType"].ToString(),
-    //            Address = result.Tables[0].Rows[0]["Address"].ToString(),
-    //            City = result.Tables[0].Rows[0]["City"].ToString(),
-    //            State = result.Tables[0].Rows[0]["State"].ToString(),
-
-    //        };
-    //        return View(newP);
-    //    }
-
-    //    public ActionResult Delete(int? id, string name, string type)
-    //    {
-    //        ViewBag.Message = "Your contact page.";
-
-    //        return View(new Provider() { ProviderId = id ?? 0, ProviderType = type, ProviderName = name });
-    //    }
-
-    //[HttpPost]
-    //public ActionResult Edit(Provider p)
-    //{
-    //    if (ModelState.IsValid)
-    //    {
-
-    //        Provider newP = p;
-    //        p.UpdateProvider(p);
-    //    }
-
-    //    return RedirectToAction("ShowProviders");
-    //}
-
-    //    [HttpPost]
-    //    public ActionResult Delete(Provider p)
-    //    {
-    //        if (ModelState.IsValid)
-    //        {
-
-    //            Provider.DeleteProvider(p.ProviderId);
-    //        }
-
-    //        return RedirectToAction("ShowProviders");
-    //    }
-
-    //    public ActionResult ShowProviders()
-    //    {
-    //        List<Provider> p = new List<Provider>();
-
-    //        var dataTables = Provider.GetProviderData();
-
-    //        foreach (DataRow data in dataTables.Tables[0].Rows)
-    //        {
-    //            //Provider newProvider = new Provider()
-    //            //{
-    //            //    ProviderId = Convert.ToInt32(data[0].ToString()),
-    //            //    ProviderName = data[1].ToString(),
-    //            //    ProviderType = data[2].ToString()
-    //            //};
-
-    //            //p.Add(newProvider);
-
-    //            p.Add(new Provider()
-    //            {
-    //                ProviderId = Convert.ToInt32(data[0].ToString()),
-    //                ProviderName = data[1].ToString(),
-    //                ProviderType = data[2].ToString()
-    //            });
-    //        }
-
-    //        return View(p);
-    //    }
-    //}
-
-
-
-
-
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            ViewBag.Message = Provider.GetValues();     
             return View();
         }
 
@@ -214,32 +81,32 @@ namespace MVCWebApplication22.Controllers
 
 
 
-        public ActionResult ShowProvider()
-        {
-            List<Provider> p = new List<Provider>();
+        //public ActionResult ShowProvider()
+        //{
+        //    List<Provider> p = new List<Provider>();
 
-            var dataTables = Provider.GetProviderData();
+        //    var dataTables = Provider.GetProviderData();
 
-            foreach (DataRow data in dataTables.Tables[0].Rows)
-            {
-                //Provider newProvider = new Provider()
-                //{
-                //    ProviderId = Convert.ToInt32(data[0].ToString()),
-                //    ProviderName = data[1].ToString(),
-                //    ProviderType = data[2].ToString()
-                //};
+        //    foreach (DataRow data in dataTables.Tables[0].Rows)
+        //    {
+        //        //Provider newProvider = new Provider()
+        //        //{
+        //        //    ProviderId = Convert.ToInt32(data[0].ToString()),
+        //        //    ProviderName = data[1].ToString(),
+        //        //    ProviderType = data[2].ToString()
+        //        //};
 
-                //p.Add(newProvider);
+        //        //p.Add(newProvider);
 
-                p.Add(new Provider()
-                {
-                    ProviderId = Convert.ToInt32(data[0].ToString()),
-                    ProviderName = data[1].ToString(),
-                    ProviderType = data[2].ToString()
-                });
-            }
-            return View(p);
-        }
+        //        p.Add(new Provider()
+        //        {
+        //            ProviderId = Convert.ToInt32(data[0].ToString()),
+        //            ProviderName = data[1].ToString(),
+        //            ProviderType = data[2].ToString()
+        //        });
+        //    }
+        //    return View(p);
+        //}
 
         [HttpPost]
         public ActionResult Edit(Provider p)
@@ -273,17 +140,52 @@ namespace MVCWebApplication22.Controllers
             var result = Provider.GetProviderDataByProviderId(id ?? 0);
 
 
-            Provider newP = new Provider()
-            {
-                ProviderId = Convert.ToInt32(result.Tables[0].Rows[0][0].ToString()),
-                ProviderName = result.Tables[0].Rows[0]["ProviderName"].ToString(),
-                ProviderType = result.Tables[0].Rows[0]["ProviderType"].ToString(),
-                //Address = result.Tables[0].Rows[0]["Address"].ToString(),
-                //City = result.Tables[0].Rows[0]["City"].ToString(),
-                //State = result.Tables[0].Rows[0]["State"].ToString(),
+            //Provider newP = new Provider()
+            //{
+            //    ProviderId = Convert.ToInt32(result.Tables[0].Rows[0][0].ToString()),
+            //    ProviderName = result.Tables[0].Rows[0]["ProviderName"].ToString(),
+            //    ProviderType = result.Tables[0].Rows[0]["ProviderType"].ToString(),
+            //    //Address = result.Tables[0].Rows[0]["Address"].ToString(),
+            //    //City = result.Tables[0].Rows[0]["City"].ToString(),
+            //    //State = result.Tables[0].Rows[0]["State"].ToString(),
 
-            };
-            return View(newP);
+            //};
+            //return View(newP);
+
+            return View(result);
+        }
+
+        //public ActionResult ProvidersDetails()
+        //{
+        //    List<Provider> p = new List<Provider>();
+
+        //    List<SelectListItem> providers = new List<SelectListItem>();
+
+        //    var dataTables = Provider.GetProviderData();
+
+        //    foreach (DataRow data in dataTables.Tables[0].Rows)
+        //    {
+        //        providers.Add(new SelectListItem()
+        //        {
+        //            Text = data[1].ToString(),
+        //            Value = data[0].ToString()
+        //        });
+        //    }
+
+        //    return View(providers);
+        //}
+
+
+
+
+
+        public ActionResult ShowProvider()
+        {
+            List<Provider> p = new List<Provider>();
+
+            var dataTables = Provider.GetProviderDataAsync();
+
+            return View(dataTables);
         }
 
         public ActionResult ProvidersDetails()
@@ -292,18 +194,8 @@ namespace MVCWebApplication22.Controllers
 
             List<SelectListItem> providers = new List<SelectListItem>();
 
-            var dataTables = Provider.GetProviderData();
-
-            foreach (DataRow data in dataTables.Tables[0].Rows)
-            {
-                providers.Add(new SelectListItem()
-                {
-                    Text = data[1].ToString(),
-                    Value = data[0].ToString()
-                });
-            }
-
-            return View(providers);
+            var dataTables = Provider.GetProviderDataAsync();
+            return View(dataTables);
         }
 
     }
--- MVCWebApplication22/Models/Provider.cs
+++ MVCWebApplication22/Models/Provider.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Data;
 using System.Data.SqlClient;
+using System.Net.Http;
+using System.Threading.Tasks;
+using jsonN = Newtonsoft.Json;
 
 namespace MVCWebApplication22.Models
 {
@@ -16,107 +19,81 @@ namespace MVCWebApplication22.Models
         //public string City { get; set; }
         //public String State { get; set; }
 
-        //    private static string connectionString = @"Data Source=LAPTOP-HKBN8ODT\NIDHESH;Initial Catalog=ProductDatabase;User ID=mvcdev;Password=testing2";
+        //private static string connectionString = @"Data Source=DESKTOP-37UDTFK\VIKASSQL;Initial Catalog=ProductDatabase;User ID=mvcdev;Password=testing";
+        //public static DataSet GetProviderData()
+        //{
+
+        //    SqlConnection conn = new SqlConnection(connectionString);
 
 
-        //    public static DataSet GetProviderData()
-        //    {
-        //        SqlConnection conn = new SqlConnection(connectionString);
-
-        //        conn.Open();
-        //        SqlCommand command = new SqlCommand("select * from Provider", conn);
-        //        SqlDataAdapter adaptor = new SqlDataAdapter(command);
-        //        DataSet ds = new DataSet();
-        //        adaptor.Fill(ds);
-        //        return ds;
-        //    }
-
-        //    public static DataSet GetProviderDataByProviderId(int id)
-        //    {
-        //        SqlConnection conn = new SqlConnection(connectionString);
-
-        //        conn.Open();
-        //        SqlCommand command = new SqlCommand($"select * from Provider where providerid = {id}", conn);
-        //        SqlDataAdapter adaptor = new SqlDataAdapter(command);
-        //        DataSet ds = new DataSet();
-        //        adaptor.Fill(ds);
-        //        return ds;
-        //    }
-
-        //    internal void InsertProvider(Provider p)
-        //    {
-        //        SqlConnection conn = new SqlConnection(connectionString);
-
-        //        string insertCmd = $"insert into Provider (ProviderName, ProviderType) values ('{p.ProviderName}','{p.ProviderType}')";
-        //        conn.Open();
-        //        SqlCommand command = new SqlCommand(insertCmd, conn);
-        //        command.ExecuteNonQuery();
-        //    }
-
-        //    internal void UpdateProvider(Provider p)
-        //    {
-        //        SqlConnection conn = new SqlConnection(connectionString);
-
-        //        string insertCmd = $"update provider set providertype='{p.ProviderType}', providername='{p.ProviderName}' where providerid={p.ProviderId}";
-        //        conn.Open();
-        //        SqlCommand command = new SqlCommand(insertCmd, conn);
-        //        command.ExecuteNonQuery();
-        //    }
-
-        //    internal static void DeleteProvider(int p)
-        //    {
-        //        SqlConnection conn = new SqlConnection(connectionString);
-
-        //        string insertCmd = $"Delete provider where providerid={p}";
-        //        conn.Open();
-        //        SqlCommand command = new SqlCommand(insertCmd, conn);
-        //        command.ExecuteNonQuery();
-        //    }
+        //    conn.Open();
+        //    SqlCommand command = new SqlCommand("select * from Provider", conn);
+        //    SqlDataAdapter adaptor = new SqlDataAdapter(command);
+        //    DataSet ds = new DataSet();
+        //    adaptor.Fill(ds);
+        //    return ds;
         //}
 
 
+        private static string connectionString = @"Data Source=DESKTOP-37UDTFK\VIKASSQL;Initial Catalog=ProductDatabase;User ID=mvcdev;Password=testing";
+        private const string baseAddress = "http://localhost/FirstWebApi/";
+
+        public static List<Provider> GetProviderDataAsync()
+        {
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost/FirstWebApi/");
+            var response = client.GetAsync("api/provider");                       // create a call to api
+            var value = response.Result.Content.ReadAsStringAsync().Result;       // getting response i.e value
 
 
-        public static DataSet GetProviderData()
+            var providerResult = jsonN.JsonConvert.DeserializeObject<List<Provider>>(value);
+
+            return providerResult;
+        }
+
+        public static Provider GetProviderDataByProviderId(int id)
         {
 
-            var connectionString = @"Data Source=DESKTOP-37UDTFK\VIKASSQL;Initial Catalog=ProductDatabase;User ID=mvcdev;Password=testing";
-            SqlConnection conn = new SqlConnection(connectionString);
+            //SqlConnection conn = new SqlConnection(connectionString);
 
+            //conn.Open();
+            //SqlCommand command = new SqlCommand($"select * from Provider where providerid = {id}", conn);
+            //SqlDataAdapter adaptor = new SqlDataAdapter(command);
+            //DataSet ds = new DataSet();
+            //adaptor.Fill(ds);
+            //return ds;
 
-            conn.Open();
-            SqlCommand command = new SqlCommand("select * from Provider", conn);
-            SqlDataAdapter adaptor = new SqlDataAdapter(command);
-            DataSet ds = new DataSet();
-            adaptor.Fill(ds);
-            return ds;
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost/FirstWebApi/");
+
+            var response = client.GetAsync("api/provider/" + id);
+
+            var providerResult = jsonN.JsonConvert.DeserializeObject<Provider>(response.Result.Content.ReadAsStringAsync().Result);
+
+            return providerResult;
         }
+
 
         internal void InsertProvider(Provider p)
         {
-            var connectionString = @"Data Source=DESKTOP-37UDTFK\VIKASSQL;Initial Catalog=ProductDatabase;User ID=mvcdev;Password=testing";
-            SqlConnection conn = new SqlConnection(connectionString);
 
-            string insertCmd = $"insert into Provider (ProviderName, ProviderType) values ('{p.ProviderName}','{p.ProviderType}')";
-            conn.Open();
-            SqlCommand command = new SqlCommand(insertCmd, conn);
-            command.ExecuteNonQuery();
+            //SqlConnection conn = new SqlConnection(connectionString);
+
+            //string insertCmd = $"insert into Provider (ProviderName, ProviderType) values ('{p.ProviderName}','{p.ProviderType}')";
+            //conn.Open();
+            //SqlCommand command = new SqlCommand(insertCmd, conn);
+            //command.ExecuteNonQuery();
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost/FirstWebApi/");
+
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.PostAsJsonAsync<Provider>("api/provider", p);
         }
-
-        //internal void UpdateProvider(Provider p)
-        //{
-        //    var connectionString = @"Data Source=DESKTOP-37UDTFK\VIKASSQL;Initial Catalog=ProductDatabase;User ID=mvcdev;Password=testing";
-        //    SqlConnection conn = new SqlConnection(connectionString);
-
-        //    string insertCmd = $"UPDATE [dbo].[Provider] SET [ProviderName] = {p.ProviderName},[ProviderType] = {p.ProviderType},[CreateDate] = '{System.DateTime.Now}' WHERE ProviderId = {p.ProviderId}";
-        //    conn.Open();
-        //    SqlCommand command = new SqlCommand(insertCmd, conn);
-        //    command.ExecuteNonQuery();
-        //}
 
         internal void UpdateProvider(Provider p)
         {
-            var connectionString = @"Data Source=DESKTOP-37UDTFK\VIKASSQL;Initial Catalog=ProductDatabase;User ID=mvcdev;Password=testing";
+          
             SqlConnection conn = new SqlConnection(connectionString);
 
             string insertCmd = $"update provider set providertype='{p.ProviderType}', providername='{p.ProviderName}' where providerid={p.ProviderId}";
@@ -125,39 +102,29 @@ namespace MVCWebApplication22.Models
             command.ExecuteNonQuery();
         }
 
-        //internal static void DeleteProvider(int p)
-        //{
-        //    var connectionString = @"Data Source=DESKTOP-37UDTFK\VIKASSQL;Initial Catalog=ProductDatabase;User ID=mvcdev;Password=testing";
-        //    SqlConnection conn = new SqlConnection(connectionString);
-
-        //    string insertCmd = $"Delete provider where providerid={p}";
-        //    conn.Open();
-        //    SqlCommand command = new SqlCommand(insertCmd, conn);
-        //    command.ExecuteNonQuery();
-        //}
-
         internal static void DeleteProvider(int p)
         {
-            var connectionString = @"Data Source=DESKTOP-37UDTFK\VIKASSQL;Initial Catalog=ProductDatabase;User ID=mvcdev;Password=testing";
-            SqlConnection conn = new SqlConnection(connectionString);
+            //SqlConnection conn = new SqlConnection(connectionString);
 
-            string insertCmd = $"Delete provider where providerid={p}";
-            conn.Open();
-            SqlCommand command = new SqlCommand(insertCmd, conn);
-            command.ExecuteNonQuery();
+            //string insertCmd = $"Delete provider where providerid={p}";
+            //conn.Open();
+            //SqlCommand command = new SqlCommand(insertCmd, conn);
+            //command.ExecuteNonQuery();
+
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost/FirstWebApi/");
+
+            //client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            client.DeleteAsync("api/provider"+p);
+
+
         }
 
-        public static DataSet GetProviderDataByProviderId(int id)
+        internal static string GetValues()                      
         {
-            var connectionString = @"Data Source=DESKTOP-37UDTFK\VIKASSQL;Initial Catalog=ProductDatabase;User ID=mvcdev;Password=testing";
-            SqlConnection conn = new SqlConnection(connectionString);
-
-            conn.Open();
-            SqlCommand command = new SqlCommand($"select * from Provider where providerid = {id}", conn);
-            SqlDataAdapter adaptor = new SqlDataAdapter(command);
-            DataSet ds = new DataSet();
-            adaptor.Fill(ds);
-            return ds;
-        }
+            HttpClient client = new HttpClient();
+            var result = client.GetAsync("http://localhost/FirstWebApi/ValueApi/SearchOrders/12");
+            return result.Result.Content.ReadAsStringAsync().Result;
+        }               
     }
 }
